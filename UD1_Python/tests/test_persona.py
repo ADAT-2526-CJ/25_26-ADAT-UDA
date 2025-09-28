4f4eb6c99c2a2a38dfdf963de754b2e3bfb394c1
Urtzi Diaz Arberas <urtzid@gmail.com>
2025-09-28 22:49:27 +0200

    FIX: Test CI 2

--- UD1_Python/tests/test_persona.py
+++ UD1_Python/tests/test_persona.py
@@ -3,7 +3,7 @@ Tests con pytest para la clase Persona (Ejercicios 7 y 8).
 Cada test muestra feedback directo.
 """
 
-from ejercicio07 import Persona
+from UD1_Python.ejercicio07 import Persona
 
 
 def test_mayor_de_edad():
