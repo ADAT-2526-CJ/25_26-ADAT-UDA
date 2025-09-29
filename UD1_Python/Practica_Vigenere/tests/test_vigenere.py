0723dda385c49a7aa8b4ce7f3aa9c5c1d9b1870c
Urtzi Diaz Arberas <urtzid@gmail.com>
2025-09-29 12:53:16 +0200

    CI: Refactor test

--- UD1_Python/Practica_Vigenere/tests/test_vigenere.py
+++ UD1_Python/Practica_Vigenere/tests/test_vigenere.py
@@ -1,8 +1,18 @@
-import pytest
-
+"""
+Prueba que cifrar y luego descifrar devuelve el texto original.
+Revisa que los módulos, clases y funciones tengan docstring.
+Verifica que sigan PEP 257 (mayúscula inicial, punto final).
+Revisa que los parámetros de cada función estén documentados en el docstring.          
+"""
 from UD1_Python.Practica_Vigenere.src.vigenere import cifrar_vigenere, descifrar_vigenere
 
 def test_cifrado_y_descifrado():
+    """Prueba que cifrar y luego descifrar devuelve el texto original.
+    Arguments:
+        None
+    Returns:
+        None    
+    """
     texto = "ATAQUE"
     clave = "CLAVE"
     cifrado = cifrar_vigenere(texto, clave)
