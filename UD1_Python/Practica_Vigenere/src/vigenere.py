6fa5b75dff99e7a23bd97ff4a5015d1d5301b544
Urtzi Diaz Arberas <urtzid@gmail.com>
2025-10-02 08:55:38 +0200

    TEST: main py

---
+++ UD1_Python/Practica_Vigenere/src/vigenere.py
@@ -0,0 +1,13 @@
+"""
+Práctica _ Cifrado y Descifrado con el algoritmo de Vigenère.    
+Alumno/a: Urtzi Diaz Arberas
+Unidad Didáctica: UD1 - Python
+
+Implementación del cifrado Vigenère con ejemplos de uso en ficheros.
+
+Incluye:
+- Funciones para cifrar y descifrar textos.
+- Funciones auxiliares para leer y escribir ficheros.
+- Control de errores y codificación.
+- Ejemplo de uso en la función main().
+"""
